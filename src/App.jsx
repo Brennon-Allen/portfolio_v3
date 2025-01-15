ce118e9a1b7ba0d0d57e34b18f1892dc4db25eda
Brennon Allen <brennon.t.allen@gmail.com>
2025-01-15 12:12:38 -0600

    initial

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import profilePicture from "./assets/Pro-pic-with-border.png";
+import allenITFlyer from "./assets/Flyer.jpg";
+import VideoPlayer from "./components/VideoPlayer";
+import codeTailorsDemo from "./assets/Code Tailors Demo.mp4";
+import teamDominionDemo from "./assets/Team Dominion Demo.mp4";
+import stcDemo from "./assets/STC Demo.mp4";
+import calculationsDemo from "./assets/Calculator Demo.mp4";
+import jkrDesign from "./assets/JKR1.jpg";
+import fbiDemo from "./assets/FBI API Demo.mp4";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={profilePicture} alt="image" width={"265px"} />
+        <h2>
+          Hi, I'm <span>Brennon</span>.
+        </h2>
+        <p>
+          Welcome to my portfolio, created with <strong>Vite</strong> and
+          developed in <strong>React</strong>.
+        </p>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" width={"75px"} />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+            width={"75px"}
+          />
+        </a>
+        <h2>Portfolio Pieces</h2>
+        <div>
+          <div>
+            <p>
+              <strong>#1</strong> Basic HTML/CSS/JavaScript Website Demo (click
+              to play/pause)
+            </p>
+            <VideoPlayer videoFile={stcDemo} fileType={"video/mp4"} />
+          </div>
+          <div>
+            <p>
+              <strong>#2</strong> Basic HTML/CSS/JavaScript Website Demo (click
+              to play/pause)
+            </p>
+            <VideoPlayer videoFile={codeTailorsDemo} fileType={"video/mp4"} />
+          </div>
+          <div>
+            <p>
+              <strong>#3</strong> Basic HTML/CSS/JavaScript Website Demo (click
+              to play/pause)
+            </p>
+            <VideoPlayer videoFile={teamDominionDemo} fileType={"video/mp4"} />
+          </div>
+          <div>
+            <p>
+              <strong>#4</strong> Basic Calculations Demo (click to play/pause)
+            </p>
+            <VideoPlayer videoFile={calculationsDemo} fileType={"video/mp4"} />
+          </div>
+          <div>
+            <p>
+              <strong>#5</strong> FBI API Integration Demo (click to play/pause)
+            </p>
+            <VideoPlayer videoFile={fbiDemo} fileType={"video/mp4"} />
+          </div>
+          <div>
+            <p>
+              <strong>#6</strong> Mobile App Design
+            </p>
+            <img src={jkrDesign} alt="Flyer" width={"350px"} />
+          </div>
+          <div>
+            <p>
+              <strong>#7</strong> Flyer Design
+            </p>
+            <img src={allenITFlyer} alt="Flyer" width={"350px"} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/VideoPlayer.jsx
@@ -0,0 +1,31 @@
+import React, { useRef } from "react";
+
+const VideoPlayer = ({ videoFile, fileType }) => {
+  const videoRef = useRef(null);
+
+  const handlePlayPause = () => {
+    if (videoRef.current) {
+      if (videoRef.current.paused) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+      }
+    }
+  };
+
+  return (
+    <div>
+      <video
+        ref={videoRef}
+        width="350px"
+        onClick={handlePlayPause}
+        style={{ cursor: "pointer" }}
+      >
+        <source src={videoFile} type={fileType} />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+export default VideoPlayer;
